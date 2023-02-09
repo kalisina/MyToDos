1f253f9e9179d805f93261ec6681f4ce55e91cb2
Kali Sina <ali.sina89@yahoo.com>
2023-02-09 12:50:16 +0100

    Initial Commit

---
+++ MyToDos/MyToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosApp.swift
+//  MyToDos
+//
+//  Created by Triumph on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
